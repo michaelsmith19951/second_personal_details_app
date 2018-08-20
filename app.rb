13d62f39d69a287e5d878db6c24423e615713649
Michael Smith <michaelsmith19951@yahoo.com>
2018-08-20 15:27:14 -0400

    Added final page, results page, fixed bugs

--- app.rb
+++ app.rb
@@ -150,8 +150,8 @@ get '/math_operations_page' do
 	favorite_third_number = params[:favorite_third_number]
 	math_operation = params[:math_operation]
 	p "test that first_name_input_box is #{first_name_input_box} on get math_operations_page"
-	p "test that last_name_input_box is #{last_name_input_box} on get math_operationsr_page"
-	p "test that favorite_animal is #{favorite_animal} on get math_operations_pagee"
+	p "test that last_name_input_box is #{last_name_input_box} on get math_operations_page"
+	p "test that favorite_animal is #{favorite_animal} on get math_operations_page"
 	p "test that favorite_color is #{favorite_color} on get math_operations_page"
 	p "test that favorite_first_number is #{favorite_first_number} on get math_operations_page"
 	p "test that favorite_second_number is #{favorite_second_number} on get math_operations_page"
@@ -166,7 +166,7 @@ get '/math_operations_page' do
 	when "divide"
 		math_operation = "/"
 	end
-	erb :math_operations_page, locals:{first_name_input_box: params[:first_name_input_box], last_name_input_box: params[:last_name_input_box], favorite_animal: params[:favorite_animal], favorite_color: params[:favorite_color], favorite_first_number: params[:favorite_first_number], favorite_second_number: params[:favorite_second_number], favorite_third_number: params[:favorite_third_number]}
+	erb :math_operations_page, locals:{first_name_input_box: params[:first_name_input_box], last_name_input_box: params[:last_name_input_box], favorite_animal: params[:favorite_animal], favorite_color: params[:favorite_color], favorite_first_number: params[:favorite_first_number], favorite_second_number: params[:favorite_second_number], favorite_third_number: params[:favorite_third_number], math_operation: params[:math_operation]}
 end
 post '/math_operations_page' do
 	first_name_input_box = params[:first_name_input_box]
@@ -178,20 +178,72 @@ post '/math_operations_page' do
 	favorite_third_number = params[:favorite_third_number]
 	math_operation = params[:math_operation]
 	p "test that first_name_input_box is #{first_name_input_box} on post math_operations_page"
-	p "test that last_name_input_box is #{last_name_input_box} on post math_operationsr_page"
-	p "test that favorite_animal is #{favorite_animal} on post math_operations_pagee"
+	p "test that last_name_input_box is #{last_name_input_box} on post math_operations_page"
+	p "test that favorite_animal is #{favorite_animal} on post math_operations_page"
 	p "test that favorite_color is #{favorite_color} on post math_operations_page"
 	p "test that favorite_first_number is #{favorite_first_number} on post math_operations_page"
 	p "test that favorite_second_number is #{favorite_second_number} on post math_operations_page"
 	p "test that favorite_third_number is #{favorite_third_number} on post math_operations_page"
 	p "test that math_operation is #{math_operation} on post math_operations_page"
 	if math_operation == "+"
-		math_operation = "plus"
+		math_operation = "addition"
+	end
+	if math_operation == "-"
+		math_operation = "subtraction"
+	end
+	if math_operation == "*"
+		math_operation = "multiplication"
+	end
+	if math_operation == "/"
+		math_operation = "division"
 	end
 	if math_operation == "/" && num2 == 0
 		p "You can't divide by zero"
 		nil
 	else
 	end
-	redirect '/final_page?first_name_input_box=' + first_name_input_box + '&last_name_input_box=' + last_name_input_box + '&favorite_animal=' + favorite_animal + '&favorite_color=' + favorite_color + '&favorite_first_number=' + favorite_first_number + '&favorite_second_number=' + favorite_second_number + '&favorite_third_number=' + favorite_third_number + '&math_operation=' + math_operation
+	redirect '/results_page?first_name_input_box=' + first_name_input_box + '&last_name_input_box=' + last_name_input_box + '&favorite_animal=' + favorite_animal + '&favorite_color=' + favorite_color + '&favorite_first_number=' + favorite_first_number + '&favorite_second_number=' + favorite_second_number + '&favorite_third_number=' + favorite_third_number + '&math_operation=' + math_operation
 end
+get '/results_page' do
+	p "#{params} on results page"
+	first_name_input_box = params[:first_name_input_box]
+	last_name_input_box = params[:last_name_input_box]
+	favorite_animal = params[:favorite_animal]
+	favorite_color = params[:favorite_color]
+	favorite_first_number = params[:favorite_first_number]
+	favorite_second_number = params[:favorite_second_number]
+	favorite_third_number = params[:favorite_third_number]
+	math_operation = params[:math_operation]
+	result_operation = calculate(math_operation, favorite_first_number, favorite_second_number, favorite_third_number)
+	p "test that first_name_input_box is #{first_name_input_box} on get results_page"
+	p "test that last_name_input_box is #{last_name_input_box} on get results_page"
+	p "test that favorite_animal is #{favorite_animal} on get results_page"
+	p "test that favorite_color is #{favorite_color} on get results_page"
+	p "test that favorite_first_number is #{favorite_first_number} on get results_page"
+	p "test that favorite_second_number is #{favorite_second_number} on get results_page"
+	p "test that favorite_third_number is #{favorite_third_number} on get results_page"
+	p "test that math_operation is #{math_operation} on get results_page"
+	p "test that result_operation is #{result_operation} on get results_page"
+	erb :results_page, locals:{first_name_input_box: params[:first_name_input_box], last_name_input_box: params[:last_name_input_box], favorite_animal: params[:favorite_animal], favorite_color: params[:favorite_color], favorite_first_number: params[:favorite_first_number], favorite_second_number: params[:favorite_second_number], favorite_third_number: params[:favorite_third_number], math_operation: math_operation, result_operation: result_operation}
+end
+# post '/results_page' do
+# 	first_name_input_box = params[:first_name_input_box]
+# 	last_name_input_box = params[:last_name_input_box]
+# 	favorite_animal = params[:favorite_animal]
+# 	favorite_color = params[:favorite_color]
+# 	favorite_first_number = params[:favorite_first_number]
+# 	favorite_second_number = params[:favorite_second_number]
+# 	favorite_third_number = params[:favorite_third_number]
+# 	math_operation = params[:math_operation]
+# 	result_operation = calculate(math_operation, num1, num2, num3)
+# 	p "test that first_name_input_box is #{first_name_input_box} on post results_page"
+# 	p "test that last_name_input_box is #{last_name_input_box} on post results_page"
+# 	p "test that favorite_animal is #{favorite_animal} on post results_page"
+# 	p "test that favorite_color is #{favorite_color} on post results_page"
+# 	p "test that favorite_first_number is #{favorite_first_number} on post results_page"
+# 	p "test that favorite_second_number is #{favorite_second_number} on post results_page"
+# 	p "test that favorite_third_number is #{favorite_third_number} on post results_page"
+# 	p "test that math_operation is #{math_operation} on post results_page"
+# 	p "test that result is #{result} on post results_page"
+# 	redirect '/results_page'
+# end
--- math_functions.rb
+++ math_functions.rb
@@ -1,71 +1,74 @@
 # Function that performs addition
-def addition(first_number, second_number)
+def addition(first_number, second_number, third_number)
 # Conditional that prevents entering of strings and arrays
-if (first_number == "") || (second_number == "")
+if (first_number == "") || (second_number == "") || (third_number == "")
 	nil
-elsif (first_number == []) || (second_number == [])
+elsif (first_number == []) || (second_number == []) || (third_number == [])
 	nil
 else
-	answer = first_number + second_number
+	result = first_number + second_number + third_number
 end
 
-	answer
+	result
 end
 
 # Function that performs subtraction
-def subtraction(first_number, second_number)
-	if (first_number == "") || (second_number == "")
+def subtraction(first_number, second_number, third_number)
+	if (first_number == "") || (second_number == "") || (third_number == "")
 	nil
-elsif (first_number == []) || (second_number == [])
+elsif (first_number == []) || (second_number == [])  || (third_number == [])
 	nil
 else
-	answer = first_number - second_number
+	result = first_number - second_number - third_number
 end
-	answer
+	result
 end
 
 # Function that performs multiplication
-def multiplication(first_number, second_number)
-		if (first_number == "") || (second_number == "")
+def multiplication(first_number, second_number, third_number)
+		if (first_number == "") || (second_number == "") || (third_number == "")
 	nil
-elsif (first_number == []) || (second_number == [])
+elsif (first_number == []) || (second_number == [])  || (third_number == [])
 	nil
 else
-	answer = first_number * second_number
+	result = first_number * second_number * third_number
 end
-	answer
+	result
 end
 
 # Function that performs division
-def division(first_number, second_number)
+def division(first_number, second_number, third_number)
 # Conditional that removes divide by zero error
-if second_number == 0
-answer = "You can't divide by zero"
+		if second_number == 0
+	result = "You can't divide by zero"
+elsif third_number == 0
+	result = "You can't divide by zero"
 else
-if (first_number == "") || (second_number == "")
+				if (first_number == "") || (second_number == "") || (third_number == "")
 	nil
-elsif (first_number == []) || (second_number == [])
+elsif (first_number == []) || (second_number == []) || (third_number == [])
 	nil
 else
-	answer = first_number / second_number
+	result = first_number / second_number / third_number
 end
 end
-	answer
+	result
 
 end
 
 
-def calculate(math_operation, num1, num2)
-	num1 = num1.to_i
-	num2 = num2.to_i
+def calculate(math_operation, favorite_first_number, favorite_second_number, favorite_third_number)
+	favorite_first_number = favorite_first_number.to_i
+	favorite_second_number = favorite_second_number.to_i
+	favorite_third_number = favorite_third_number.to_i
 	case math_operation
-	when "+"
-		return addition(num1, num2)
-	when "-"
-		return subtraction(num1, num2)
-	when "*"
-		return multiplication(num1, num2)
-	when "/"
-		return division(num1, num2)
+	when "addition"
+		return addition(favorite_first_number, favorite_second_number, favorite_third_number)
+	when "subtraction"
+		return subtraction(favorite_first_number, favorite_second_number, favorite_third_number)
+	when "multiplication"
+		return multiplication(favorite_first_number, favorite_second_number, favorite_third_number)
+	when "division"
+		return division(favorite_first_number, favorite_second_number, favorite_third_number)
 	end
 end
